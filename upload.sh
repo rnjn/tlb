3a944fecbd960851bb58151d1050932bccde3fce
Pavan Sudarshan <itspanzi@gmail.com>
2011-05-22 22:03:10 +0530

    Added a MD5 check to make sure we have uploaded the file properly by downloading and checking with the original file

--- upload.sh
+++ upload.sh
@@ -5,4 +5,17 @@ fi
 
 TLB_UPLOAD_PASSWORD=$1 TLB_UPLOAD_FILE_PATH=$2 TLB_UPLOAD_TARGET_FILE_NAME=$3 TLB_UPLOAD_SUMMARY="$4" TLB_UPLOAD_LABELS="$5" ant -Doffline=true upload-files
 
-curl "http://
+tmp_file=/tmp/$3
+
+rm $tmp_file
+curl "http://tlb.googlecode.com/files/$3" > $tmp_file
+
+actual_md5=`md5sum ${tmp_file} | awk '{ print $1 }'`
+expected_md5=`md5sum $2 | awk '{print $1 }'`
+
+if [[ $actual_md5 != $expected_md5 ]]; then
+  echo "Looks like the file upload is wrong. Expected MD5: $expected_md5, but got: $actual_md5"
+  exit 1
+fi
+
+echo "Uploaded file successfully"
